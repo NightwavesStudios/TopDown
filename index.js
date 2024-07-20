33cf696a2debb15b3db73f057292937b98a8a914
Chris Henry <151556196+NightwavesStudios@users.noreply.github.com>
2024-07-19 20:44:40 -0400

    Update JS

--- index.js
+++ index.js
@@ -1,5 +1,175 @@
-/** Setup **/
-var canvas = document.getElementById("canvas");
-canvas.width = 200;
-canvas.height = 200;
+const gameState = {};
+
+class GameScene extends Phaser.Scene {
+
+  constructor() {
+    super({ key: 'GameScene' });
+  }
+
+  preload() {
+
+    //Load Background Image
+    this.load.image('cave', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/cave_background.png');
+
+    //Load Platform Image
+    this.load.image('platform', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/platform.png');
+
+    //Load Player Spritesheet
+    this.load.spritesheet('codey', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/codey_sprite.png', { frameWidth: 72, frameHeight: 90 });
+
+    //Load Enemy Spritesheet
+    this.load.spritesheet('snowman', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/snowman.png', { frameWidth: 50, frameHeight: 70 });
+
+    // Load Exit Spritesheet 
+    this.load.spritesheet('exit', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/cave_exit.png', { frameWidth: 60, frameHeight: 70 });
+  }
+
+  create() {
+    gameState.active = true;
+
+    //Draw Game Background
+    this.add.image(0, 0, 'cave').setOrigin(0, 0);
+
+    //Create Platforms
+    const platforms = this.physics.add.staticGroup();
+
+    //Platform Positions
+    const platPositions = [
+      { x: 50, y: 575 }, { x: 250, y: 575 }, { x: 450, y: 575 }, { x: 400, y: 380 }, { x: 100, y: 200 }
+    ];
+    platPositions.forEach(plat => {
+      platforms.create(plat.x, plat.y, 'platform')
+    });
+
+    //Draw Sprite
+    gameState.player = this.physics.add.sprite(50, 500, 'codey').setScale(.8)
+
+    //Add Collisions
+    this.physics.add.collider(gameState.player, platforms);
+    gameState.player.setCollideWorldBounds(true);
+
+    gameState.cursors = this.input.keyboard.createCursorKeys();
+
+    //Player Run Animation
+    this.anims.create({
+      key: 'run',
+      frames: this.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
+      frameRate: 5,
+      repeat: -1
+    });
+
+    //Player Idle Animation
+    this.anims.create({
+      key: 'idle',
+      frames: this.anims.generateFrameNumbers('codey', { start: 4, end: 5 }),
+      frameRate: 5,
+      repeat: -1
+    });
+
+    //Create Enemy
+    gameState.enemy = this.physics.add.sprite(400, 300, 'snowman');
+
+    //Add Collisions for Enemy
+    platforms
+    this.physics.add.collider(gameState.enemy, platforms);
+  
+    //Snowman Active Animation
+    this.anims.create({
+      key: 'snowmanAlert',
+      frames: this.anims.generateFrameNumbers('snowman', { start: 0, end: 3 }),
+      frameRate: 4,
+      repeat: -1
+    });
+    
+    //Play Snowman Active Animation
+    gameState.enemy.anims.play('snowmanAlert', true);
+
+    //End Game When Player Touches Enemy
+    this.physics.add.overlap(gameState.player, gameState.enemy, () => {
+
+      //Draw Game Over
+      this.add.text(150, 50, '      Game Over...\n  Click to play again.', { fontFamily: 'Arial', fontSize: 36, color: '#ffffff' });
+
+      //Pause Game
+      this.physics.pause();
+      gameState.active = false;
+      this.anims.pauseAll();
+      
+      //Restart Game On Mouse Click
+      this.input.on('pointerup', () => {
+        this.scene.restart();
+      })
+    });
+
+    gameState.exit = this.physics.add.sprite(50, 142, 'exit');
+    this.anims.create({
+      key: 'glow',
+      frames: this.anims.generateFrameNumbers('exit', { start: 0, end: 5 }),
+      frameRate: 4,
+      repeat: -1
+    });
+    this.physics.add.collider(gameState.exit, platforms);
+    gameState.exit.anims.play('glow', true);
+
+    // Adds a win condition
+    this.physics.add.overlap(gameState.player, gameState.exit, () => {
+      this.add.text(150, 50, 'You reached the exit!\n  Click to play again.', { fontFamily: 'Arial', fontSize: 36, color: '#ffffff' });
+      this.physics.pause();
+      gameState.active = false;
+      this.anims.pauseAll();
+      // Add your code below for step 2:
+      
+      
+      
+      this.input.on('pointerup', () => {
+        this.anims.resumeAll();
+        this.scene.restart();
+      })
+    })
+    
+    // Add your code below for step 1:
+    
+    
+  }
+
+  update() {
+    if (gameState.active) {
+      if (gameState.cursors.right.isDown) {
+        gameState.player.setVelocityX(350);
+        gameState.player.anims.play('run', true);
+        gameState.player.flipX = false;
+      } else if (gameState.cursors.left.isDown) {
+        gameState.player.setVelocityX(-350);
+        gameState.player.anims.play('run', true);
+        gameState.player.flipX = true;
+      } else {
+        gameState.player.setVelocityX(0);
+        gameState.player.anims.play('idle', true);
+      }
+      // Codey jumps if they are touching the ground and either the space bar or up arrow key is pressed
+      if ((gameState.cursors.space.isDown || gameState.cursors.up.isDown)&& gameState.player.body.touching.down) {
+        gameState.player.anims.play('jump', true);
+        gameState.player.setVelocityY(-800);
+      }
+    }
+  }
+}
+
+
+const config = {
+  type: Phaser.AUTO,
+  width: 500,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 1500 },
+      enableBody: true,
+    }
+  },
+  scene: [GameScene]
+};
+
+const game = new Phaser.Game(config);
+
 
